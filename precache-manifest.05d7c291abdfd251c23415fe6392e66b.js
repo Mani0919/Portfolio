9342a7627f1013e2a4a61f8d1f373106f4c0f314
Mani0919 <157208208+Mani0919@users.noreply.github.com>
2024-02-02 00:31:30 +0530

    Add files via upload

---
+++ precache-manifest.05d7c291abdfd251c23415fe6392e66b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ef4ceac8a4abaab4e36e29b6a013e66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8c62a560f3c374515ac",
+    "url": "/static/css/main.b92e36fb.chunk.css"
+  },
+  {
+    "revision": "5a82ecaa4e024e9d8fc5",
+    "url": "/static/js/2.589a18eb.chunk.js"
+  },
+  {
+    "revision": "f8c62a560f3c374515ac",
+    "url": "/static/js/main.d11ce91b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "373a2221346e0774a343d9c7a6112943",
+    "url": "/static/media/1679674459767.373a2221.jpg"
+  },
+  {
+    "revision": "8c7faf7ee51dc9b97ec21fca8d46c61c",
+    "url": "/static/media/Resume(MK).8c7faf7e.pdf"
+  },
+  {
+    "revision": "fe2ce6e348936e4bd2e27d41837b53b1",
+    "url": "/static/media/aitam.fe2ce6e3.jpeg"
+  },
+  {
+    "revision": "9bfc3eb8ced7ae687767236e55ce7b6d",
+    "url": "/static/media/inter.9bfc3eb8.jpeg"
+  },
+  {
+    "revision": "2d89e9a69952d4cceda8b4ca07283f33",
+    "url": "/static/media/school.2d89e9a6.png"
+  }
+]);
